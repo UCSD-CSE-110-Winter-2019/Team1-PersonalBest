ff175dc2b548ab85999db141a6fa607afdd54e08
lee-aaron <lee.aaron.68@gmail.com>
2019-02-06 14:14:52 -0800

    Fix merge

---
+++ app/src/main/java/edu/ucsd/cse110/team1_personalbest/Firebase/Database.java
@@ -0,0 +1,55 @@
+package edu.ucsd.cse110.team1_personalbest.Firebase;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.support.constraint.Constraints.TAG;
+
+public class Database {
+
+    FirebaseFirestore db;
+
+    public Database() {
+        db = FirebaseFirestore.getInstance();
+    }
+
+    public Database(FirebaseFirestore store) {
+        db = store;
+    }
+
+    public void push() {
+// Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+// Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
+    public void get() {
+
+    }
+}
