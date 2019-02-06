9d71b56369379f19271932be493833350112edae
yuanyuan huang <yuh328@ucsd.edu>
2019-02-06 12:23:37 -0800

    create new ui

--- app/src/main/java/edu/ucsd/cse110/team1_personalbest/Activities/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/team1_personalbest/Activities/MainActivity.java
@@ -2,6 +2,7 @@ package edu.ucsd.cse110.team1_personalbest.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import edu.ucsd.cse110.team1_personalbest.R;
 
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnStartWalk = findViewById(R.id.buttonStartWalk);
     }
 }
