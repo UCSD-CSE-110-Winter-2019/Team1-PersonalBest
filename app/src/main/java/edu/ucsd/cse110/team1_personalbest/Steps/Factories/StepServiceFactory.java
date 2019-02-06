88c2e2e028f2ea3296eaa8c4d49652d79aab8b23
Kailash Jayaram <kjayaram@ucsd.edu>
2019-02-05 20:51:21 -0800

    added some backend handling for the gfit api

---
+++ app/src/main/java/edu/ucsd/cse110/team1_personalbest/Steps/Factories/StepServiceFactory.java
@@ -0,0 +1,46 @@
+package edu.ucsd.cse110.team1_personalbest.Steps.Factories;
+
+import android.app.Activity;
+
+import android.util.Log;
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.ucsd.cse110.team1_personalbest.Steps.Interfaces.StepService;
+
+/**
+ * Factory for producing instances of {@link StepService}.
+ */
+public class StepServiceFactory {
+    private static final String TAG = "[FitnessServiceFactory]";
+
+    // Map of known step service blueprints
+    private static final Map<String, BluePrint> blueprints = new HashMap<>();
+
+    /**
+     * adds a new step service to the map.
+     * @param key: string key to reference this service by
+     * @param bluePrint: a blueprint of this service
+     */
+    public static void put(final String key, final BluePrint bluePrint) {
+        blueprints.put(key, bluePrint);
+    }
+
+    /**
+     * instantiates a new step service from the given key to the given activity.
+     * @param key: the key to find the step service with
+     * @param stepCountActivity: activity to connect to
+     * @return the step service
+     */
+    public static StepService create(final String key, final Activity stepCountActivity) {
+        Log.i(TAG, String.format("creating FitnessService with key %s", key));
+        return blueprints.get(key).create(stepCountActivity);
+    }
+
+    /**
+     * BluePrint interface for step services
+     */
+    public interface BluePrint {
+        StepService create(final Activity stepCountActivity);
+    }
+}
