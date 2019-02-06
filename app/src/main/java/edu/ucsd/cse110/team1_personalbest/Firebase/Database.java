4edb09e89a99ccc54c07defc513d84020f270891
lee-aaron <lee.aaron.68@gmail.com>
2019-02-06 14:11:04 -0800

    Tested push

--- app/src/main/java/edu/ucsd/cse110/team1_personalbest/Firebase/Database.java
+++ app/src/main/java/edu/ucsd/cse110/team1_personalbest/Firebase/Database.java
@@ -3,9 +3,12 @@ package edu.ucsd.cse110.team1_personalbest.Firebase;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -26,30 +29,38 @@ public class Database {
     }
 
     public void push() {
-// Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
+        Map<String, Integer> map = new HashMap<>();
+        map.put("06", 100);
 
-// Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+        db.collection("calendar")
+                .document("02")
+                .set(map)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Successfully added info");
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
+                        Log.w(TAG, "Error adding info", e);
                     }
                 });
     }
 
     public void get() {
+        db.collection("calendar")
+                .document("02")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if ( task.isSuccessful() ) {
 
+                            Log.d(TAG, "Got document info");
+                        }
+                    }
+                });
     }
 }
--- app/src/test/java/edu/ucsd/cse110/team1_personalbest/DatabaseUnitTest.java
+++ app/src/test/java/edu/ucsd/cse110/team1_personalbest/DatabaseUnitTest.java
@@ -6,24 +6,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import edu.ucsd.cse110.team1_personalbest.Firebase.Database;
-
 public class DatabaseUnitTest {
-
-    Database db;
 
     @Before
     public void setup() {
         FirebaseFirestore mockFirestore = Mockito.mock(FirebaseFirestore.class);
-
     }
 
     @Test
     public void testPush() {
-        if ( db == null ) {
-            return;
-        }
-        db.push();
     }
 
     @Test
