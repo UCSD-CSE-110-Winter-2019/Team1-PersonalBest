cca3b35352fed7a064b05000e7dbaa9b1a976d42
lee-aaron <lee.aaron.68@gmail.com>
2019-02-06 13:07:46 -0800

    Working on Firestore

---
+++ app/src/test/java/edu/ucsd/cse110/team1_personalbest/DatabaseUnitTest.java
@@ -0,0 +1,33 @@
+package edu.ucsd.cse110.team1_personalbest;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import edu.ucsd.cse110.team1_personalbest.Firebase.Database;
+
+public class DatabaseUnitTest {
+
+    Database db;
+
+    @Before
+    public void setup() {
+        FirebaseFirestore mockFirestore = Mockito.mock(FirebaseFirestore.class);
+        
+    }
+
+    @Test
+    public void testPush() {
+        if ( db == null ) {
+            return;
+        }
+        db.push();
+    }
+
+    @Test
+    public void testGet() {
+
+    }
+}
