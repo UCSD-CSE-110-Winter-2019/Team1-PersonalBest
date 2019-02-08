9711aac5220ef19e2569e5b01055a4342420f7a6
lee-aaron <lee.aaron.68@gmail.com>
2019-02-08 14:47:40 -0800

    Updates

--- app/src/test/java/edu/ucsd/cse110/team1_personalbest/DatabaseUnitTest.java
+++ app/src/test/java/edu/ucsd/cse110/team1_personalbest/DatabaseUnitTest.java
@@ -13,19 +13,13 @@ import edu.ucsd.cse110.team1_personalbest.Firebase.Database;
 
 public class DatabaseUnitTest {
 
-    Database db;
-
     @Before
     public void setup() {
-        FirebaseFirestore mock = Mockito.mock(FirebaseFirestore.class);
-        db = new Database(mock);
     }
 
     @Test
     public void testPush() {
-        Map<String, Integer> map = new HashMap<>();
-        map.put("06", 100);
-        db.push("02", map);
+
     }
 
     @Test
