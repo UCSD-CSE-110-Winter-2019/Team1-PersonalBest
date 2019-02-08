e7a7f3e7d0e1c7db79e1e8baca8a8d4fb9481a25
ChongGu <chg056@ucsd.edu>
2019-02-08 02:32:17 -0800

    create function for double encouragement

--- app/src/main/java/edu/ucsd/cse110/team1_personalbest/CountStepActivity.java
+++ app/src/main/java/edu/ucsd/cse110/team1_personalbest/CountStepActivity.java
@@ -1,9 +1,11 @@
 package edu.ucsd.cse110.team1_personalbest;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class CountStepActivity extends AppCompatActivity {
 
@@ -20,5 +22,15 @@ public class CountStepActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    /* Receive encouragement for doubling steps */
+    public void showEncouragementForDouble(int CurrentStep){
+        Context context = getApplicationContext();
+        CharSequence text = "You've nearly doubled your steps. Keep up the good work!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
