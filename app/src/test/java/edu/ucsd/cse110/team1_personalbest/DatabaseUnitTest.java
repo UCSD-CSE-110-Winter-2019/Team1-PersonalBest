ef3d0c9981b682f943b4a518ee908280f95e93dc
lee-aaron <lee.aaron.68@gmail.com>
2019-02-08 15:55:02 -0800

    Added JSON for storing

--- app/src/test/java/edu/ucsd/cse110/team1_personalbest/DatabaseUnitTest.java
+++ app/src/test/java/edu/ucsd/cse110/team1_personalbest/DatabaseUnitTest.java
@@ -2,28 +2,57 @@ package edu.ucsd.cse110.team1_personalbest;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import org.json.JSONObject;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
 import edu.ucsd.cse110.team1_personalbest.Firebase.Database;
 
+import static org.junit.Assert.assertEquals;
+
 public class DatabaseUnitTest {
+
+    private Database db;
+    private static String FILENAME = "test.json";
+
 
     @Before
     public void setup() {
+        db = new Database();
+    }
+
+    @After
+    public void cleanup() {
+        File temp = new File(FILENAME);
+        if ( temp.exists() ) {
+            temp.delete();
+        }
     }
 
     @Test
-    public void testPush() {
-
+    public void testWrite() {
+        try {
+            JSONObject obj = new JSONObject();
+            obj.put("test", "123");
+            db.write(FILENAME, obj);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void testGet() {
-
+        try {
+            JSONObject obj = db.read(FILENAME);
+            assertEquals(obj.toString(), "{\"test\":\"123\"}");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
