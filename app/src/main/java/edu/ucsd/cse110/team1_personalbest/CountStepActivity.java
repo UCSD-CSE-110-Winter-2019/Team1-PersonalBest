bf9c401b615a73f0f845396af6287482c2b86de6
yuanyuan huang <yuh328@ucsd.edu>
2019-02-08 00:52:28 -0800

    add new ui

--- app/src/main/java/edu/ucsd/cse110/team1_personalbest/CountStepActivity.java
+++ app/src/main/java/edu/ucsd/cse110/team1_personalbest/CountStepActivity.java
@@ -2,6 +2,8 @@ package edu.ucsd.cse110.team1_personalbest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CountStepActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class CountStepActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_count_step);
+
+        Button btnEndWalk = (Button) findViewById(R.id.buttonEndWalk);
+
+        btnEndWalk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
