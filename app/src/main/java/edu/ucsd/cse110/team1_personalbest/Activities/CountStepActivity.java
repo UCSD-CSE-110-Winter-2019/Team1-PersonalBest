6b89ac7dfa491042e2176c5c83f8bbf736092ac7
ChongGu <chg056@ucsd.edu>
2019-02-10 12:34:00 -0800

    Edit the function

--- app/src/main/java/edu/ucsd/cse110/team1_personalbest/Activities/CountStepActivity.java
+++ app/src/main/java/edu/ucsd/cse110/team1_personalbest/Activities/CountStepActivity.java
@@ -52,8 +52,21 @@ public class CountStepActivity extends AppCompatActivity {
         });
     }
 
-    /* Show an encouragement when current steps is half of the goal steps */
-    public void showEncouragementForDouble(){
+    /* Call this function to send an encouragement*/
+    public void showEncouragement(int previousSteps, int currentSteps, int goalSteps) {
+        /* When current steps is nearly doubled the previous steps*/
+        if(currentSteps >= 1.8 * previousSteps && currentSteps < 2 * previousSteps && currentSteps < goalSteps)
+            showEncouragementForNearlyDouble();
+
+        if(currentSteps >= 1.4 * previousSteps && currentSteps < 1.8 * previousSteps  && currentSteps < goalSteps)
+            showEncouragementNotDouble();
+
+        if(currentSteps >= 2 * previousSteps && currentSteps < goalSteps)
+            showEncouragementDouble();
+    }
+
+    /* Show an encouragement when current steps nearly doubled previous steps*/
+    public void showEncouragementForNearlyDouble(){
         Context context = getApplicationContext();
         CharSequence text = "You've nearly doubled your steps. Keep up the good work!";
         int duration = Toast.LENGTH_SHORT;
@@ -62,13 +75,20 @@ public class CountStepActivity extends AppCompatActivity {
         toast.show();
     }
 
-    /* Show an encouragement when click the end button and current steps is not greater than goal steps */
-    public void showEncouragementAtEnd(int currentSteps, int goalSteps){
-        // Round double value to 2 decimal points
-        double percentage = Math.round(currentSteps/goalSteps*10000.0)/100.0;
-
+    /* Show an encouragement when user significantly improve daily steps */
+    public void showEncouragementNotDouble(){
         Context context = getApplicationContext();
-        CharSequence text = "Good job! You're already at " + percentage + "% of the daily recommended number of steps.";
+        CharSequence text = "Good job! You've made great prgroess!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /* Show an encouragement when user double the step*/
+    public void showEncouragementDouble(){
+        Context context = getApplicationContext();
+        CharSequence text = "Excellent! You've doubled your steps!";
         int duration = Toast.LENGTH_SHORT;
 
         Toast toast = Toast.makeText(context, text, duration);
