07051718db1c33a4d1bb9cc87384467cae75d2e4
danmu2018 <35354854+danmu2018@users.noreply.github.com>
2019-02-11 13:22:52 -0800

    add save goal function

--- app/src/main/java/edu/ucsd/cse110/team1_personalbest/CustomGoalActivity.java
+++ app/src/main/java/edu/ucsd/cse110/team1_personalbest/CustomGoalActivity.java
@@ -1,10 +1,13 @@
 package edu.ucsd.cse110.team1_personalbest;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import edu.ucsd.cse110.team1_personalbest.Activities.MainActivity;
 
@@ -26,8 +29,30 @@ public class CustomGoalActivity extends AppCompatActivity {
         btnAcceptCustomGoal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                saveCustomGoal(v);
+                setNewGoal(v);
             }
         });
+    }
+
+    public void saveCustomGoal(View view){
+
+        EditText newGoal = (EditText) findViewById(R.id.customGoal);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("new_goal", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString("newGoal", newGoal.getText().toString());
+
+        editor.apply();
+        //Toast.makeText(MainActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+    }
+
+    public void setNewGoal(View view){
+
+        SharedPreferences sharedPreferences = getSharedPreferences("new_goal", MODE_PRIVATE);
+        String new_goal = sharedPreferences.getString("newGoal","");
+        TextView displayNewGoal = (TextView) findViewById(R.id.step_goal);
+        displayNewGoal.setText(new_goal);
     }
 }
