7cbb51b3f16b12e9a4ca800d225ca1a2943057ce
Kailash Jayaram <kjayaram@ucsd.edu>
2019-02-16 11:15:53 -0800

    changed units to miles/MPH

--- app/src/main/java/edu/ucsd/cse110/team1_personalbest/Fitness/Observers/GoogleFitnessObserver.java
+++ app/src/main/java/edu/ucsd/cse110/team1_personalbest/Fitness/Observers/GoogleFitnessObserver.java
@@ -49,6 +49,7 @@ public class GoogleFitnessObserver implements FitnessObserver {
         if(timeElapsed != null && this.speed != null && deltaDistance !=null && this.distance != null) {
             float curDistance = Float.parseFloat(this.distance.getText().toString());
             float newDistance = deltaDistance + curDistance;
+            newDistance *= 0.000621371; // convert to miles
 
             double newSpeed = 0;
             if (timeElapsed > 0) {
@@ -61,8 +62,8 @@ public class GoogleFitnessObserver implements FitnessObserver {
                     .toSeconds(timeElapsed - TimeUnit.HOURS.toMillis(hours) - TimeUnit.MINUTES.toMillis(minutes));
 
             this.timeElapsed.setText(String.format("%02d:%02d:%02d", hours, minutes, seconds));
-            this.speed.setText(String.format(Locale.ENGLISH, "%.2f", newSpeed));
-            this.distance.setText(String.format(Locale.ENGLISH, "%.2f", newDistance));
+            this.speed.setText(String.format(Locale.ENGLISH, "%.3f", newSpeed));
+            this.distance.setText(String.format(Locale.ENGLISH, "%.3f", newDistance));
         }
 
         // TODO refactor this to use simple IDatabase call
