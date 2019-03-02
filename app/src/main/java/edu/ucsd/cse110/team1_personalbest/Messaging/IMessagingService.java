b27245faf3703bf767b933fec4bfedc985c48281
Kailash Jayaram <kjayaram@ucsd.edu>
2019-03-02 15:25:28 -0800

    added some functionality for sending messages

---
+++ app/src/main/java/edu/ucsd/cse110/team1_personalbest/Messaging/IMessagingService.java
@@ -0,0 +1,16 @@
+package edu.ucsd.cse110.team1_personalbest.Messaging;
+
+import android.app.Activity;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.Query;
+
+import java.util.Map;
+
+public interface IMessagingService {
+    void sendMessage(final Map<String,String> newMessage, final EditText messageView, final String DOCUMENT_KEY);
+    void init(final TextView textView, final Query.Direction messageOrder, final String DOCUMENT_KEY);
+    void subscribe(final Activity activity, final String DOCUMENT_KEY);
+    void removeListener();
+}
---
+++ app/src/main/java/edu/ucsd/cse110/team1_personalbest/Messaging/MessagingServiceFactory.java
@@ -0,0 +1,29 @@
+package edu.ucsd.cse110.team1_personalbest.Messaging;
+
+public class MessagingServiceFactory {
+    private static boolean TEST_MODE = false;
+    private static IMessagingService test_service = null;
+    public static final String CHAT_SEPARATOR = "#";
+
+    public static void setTestService(IMessagingService service) {
+        test_service = service;
+    }
+    public static void toggleTestMode() {
+        TEST_MODE = true;
+    }
+    public static IMessagingService createMessagingService(final String u1, final String u2) {
+        if (TEST_MODE) {
+            return test_service;
+        } else {
+            return new FirestoreMessagingAdapter();
+        }
+    }
+
+    public static String getConversationKey(String u1, String u2) {
+        // just put the username that comes alphabetically first, first.
+        if(u1.compareTo(u2) < 0) {
+            return u1 + CHAT_SEPARATOR + u2;
+        }
+        return u2 + CHAT_SEPARATOR + u1;
+    }
+}
