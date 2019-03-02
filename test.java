1fbea999dfee15c8e6992e16aa4d1c1496fd8180
lee-aaron <lee.aaron.68@gmail.com>
2019-03-01 20:17:10 -0800

    Remove unnecessary file

--- test.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class test{
-    public static void main(String[] args){
-        System.out.println(Calendar.SUNDAY);
-        System.out.println(Calendar.MONDAY);
-        System.out.println(Calendar.TUESDAY);
-    }
-}
